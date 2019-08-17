b6bad29f512bbd6be969581750bd722dd80134c4
Alyson Morato <54175270+aly50n@users.noreply.github.com>
2019-08-17 15:14:44 -0300

    Add files via upload
    Questão 01 da Lista 01

---
+++ lista01ex01.py
@@ -0,0 +1,10 @@
+print("Olá! Seja Bem Vindo Ao Calculador de Combustível")
+print("Qual o seu nome?")
+n=str(input())
+print("Vamos começar", n, "!")
+c=float(input("Por favor digite o valor do litro de combustível em dinheiro-> "))
+d=float(input("Agora, me informe o valor em dinheiro que deseja abastecer-> "))
+total= d//c
+l= total
+print(n, "com este valor você obterá ", total, "litro(s) de combustível.")
+print("Obrigado por usar este aplicativo", n, "! ;)")
