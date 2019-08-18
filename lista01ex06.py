459e960523967c3c4253757e151173ead8820e33
aly50n <alyson.morato13@gmail.com>
2019-08-18 13:39:20 -0300

    lista01ex06

---
+++ lista01ex06.py
@@ -0,0 +1,6 @@
+print("Reajuste de salário do funcionário:")
+s=float(input("Digite o valor do salário atual do funcionário-> "))
+p=float(input("Digite de quantos porcento será o reajuste-> "))
+ns=print("O novo salário do funcinoário será de:", (s*p)/100+s)
+ns
+print("Obrigado por usar este aplicativo :D")
