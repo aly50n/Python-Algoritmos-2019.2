2aa49f36bc3fb11eb79b300219f1af982c0a3ffe
aly50n <alyson.morato13@gmail.com>
2019-08-18 10:45:02 -0300

    Lista01ex03

--- lista01ex03.py
+++ lista01ex03.py
@@ -1,8 +1,8 @@
 print("Seja Bem Vindo ao Calculador de dívida em atraso")
 n=input("Por favor digite o nome do devedor-> ")
-div=float(input("Digite o valor original da dívida-> "))
+div=float(input("Digite o valor original da dívida-> R$ "))
 dia=float(input("Digite a quantidade de dias de atraso no pagamento da dívida-> "))
-mul=float(input("Defina o valor da multa por dia de atraso-> "))
+mul=float(input("Defina o valor da multa por dia de atraso-> R$ "))
 result=dia*mul+div
-print(n, "está com uma dívida no valor total de", result)
+print(n, "está com uma dívida no valor total de R$", result)
 print("Obrigado por usar este aplicativo ;)")
