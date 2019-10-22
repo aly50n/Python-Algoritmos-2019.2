457d9f8aa4a6666e9755709d58ce89646ea5995f
aly50n <alyson.morato13@gmail.com>
2019-10-22 15:35:03 -0300

    LISTA05EX08

--- lista05ex08.py
+++ lista05ex08.py
@@ -6,5 +6,38 @@ for i in range(quantidade):
 for i in range(quantidade):
     lista2[i] = int(input("Digite um numéro inteiro para a lista 02: "))
 
+print("="*50)
+
+cont = 0
+dif = 1
+
 for j in range(quantidade):
-    for h in range()
+    for h in range(quantidade):
+        if lista1[j] == lista2[h]:
+            cont = cont + 1
+    if cont == quantidade:
+        dif = 0
+    if cont != quantidade:
+        dif = 1
+cont2 = 0
+dif2 = 1
+
+for j in range(quantidade):
+    for h in range(quantidade):
+        if lista2[j] == lista1[h]:
+            cont2 = cont2 + 1
+    if cont2 == quantidade:
+        dif2 = 0
+    if cont2 != quantidade:
+        dif2 = 1
+
+if dif or dif2 != 0:
+    print("As listas contêm o seu conteúdo DIFERENTE!")
+else:
+    print("As listas contêm o seu conteúdo IGUAIS!")
+
+
+print('='*50)
+
+print("Conteúdo da Lista 01:", lista1)
+print("Conteúdo da lista 02:", lista2)
