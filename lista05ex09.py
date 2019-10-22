e6f8e7e48f3a8cf1da123f02a4118a9d2efb7bfb
aly50n <alyson.morato13@gmail.com>
2019-10-22 14:42:24 -0300

    lista05ex09

--- lista05ex09.py
+++ lista05ex09.py
@@ -1,15 +1,21 @@
-quant = int(input("quantos elementos a lista vai ter? "))
-lista = [[],[]]
-valor = 0
-cont1 = 0
-cont2 = 0
-for c in range(quant):
-    valor = int(input("digite um numero: "))
-    if valor % 2 == 0:
-        lista[0] = valor
-        cont1 = cont1 + 1
-    else :
-        lista[1] = valor
+num=int(input('Digite o tamanho da lista :'))
+lista=[0]*num
+cont1=cont2=cont3=cont4=0
+
+for i in range(num):
+    lista[i]=int(input('Digite números :'))
+    if lista[i] % 2 == 0:
+        cont1= cont1 + 1
+    else:
         cont2 = cont2 + 1
-print(" total de pares: ", cont1)
-print(" total de impares: ", cont2)
+pares= [0] * cont1
+impares=[0] * cont2
+for i in range(num):
+    if lista[i] % 2 == 0:
+        pares[cont3]=lista[i]
+        cont3=cont3+1
+    else :
+        impares[cont4]=lista[i]
+        cont4=cont4 +1
+print('Valores pares', pares)
+print('Valores ímpares', impares)
