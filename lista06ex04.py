a3d385ef4ddc76ce1268a4e1d30de219b7dba64f
aly50n <alyson.morato13@gmail.com>
2019-11-09 15:40:21 -0300

    lista06ex04

---
+++ lista06ex04.py
@@ -0,0 +1,20 @@
+
+matriz = []
+for i in range(10):
+    matriz.append([0]*10)
+    
+
+for i in range(10):
+    for j in range(10):
+        if i < j:
+            matriz[i][j] = 2*i+7*j-2
+        elif i == j:
+            matriz[i][j] = 3*(i**2)-1
+        else:
+            matriz[i][j] = (4*(i**3)) - 5 * ((j**2)+1)
+
+print("Matriz")
+for i in range(10):
+    for j in range(10):
+        print(matriz[i][j], end= " ")
+    print()
