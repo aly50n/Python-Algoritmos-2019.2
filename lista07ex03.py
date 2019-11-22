c5200376015cab380ea16a0eae64a0aaf455b1df
aly50n <alyson.morato13@gmail.com>
2019-11-22 08:36:11 -0300

    lista07ex03-corrigido

--- lista07ex03.py
+++ lista07ex03.py
@@ -2,11 +2,19 @@ def ler_vetor(vetor,tamanho):
     for i in range(tamanho):
         vetor[i] = input("Digite os elementos do vetor: ")
 
+def inverter_vetor(vetor,tamanho):
+    vetorinvertido = [""]*tamanho
+    aux2 = tamanho - 1
+    for i in range(tamanho):
+        vetorinvertido[i] = vetor[aux2]
+        aux2 = aux2-1
+    return vetorinvertido
+
 tamanho = int(input("Qual o tamanho do seu vetor? "))
 vetor = [0]*tamanho
 
 ler_vetor(vetor,tamanho)
-print("Vetor principal: ", vetor)
+print("Vetor Principal: ", vetor)
 
-vetor.reverse()
-print("Vetor invertido: ", vetor)
+vetorinverso = inverter_vetor(vetor,tamanho)
+print("Vetor Invertido: ", vetorinverso)
